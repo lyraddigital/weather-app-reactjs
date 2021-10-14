20309115d63930e95eca0bc1e777940837f6d4fc
darylduck <daryl.duckmanton@gmail.com>
2021-10-15 05:56:42 +1100

    Latest

--- src/core/WeatherProvider.js
+++ src/core/WeatherProvider.js
@@ -1,26 +1,34 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
+import { WEATHER_API_URL } from './Contants';
+import { Configuration } from "./Configuration";
 import { WeatherContext } from "./WeatherContext";
+import { LocationContext } from "./LocationContext";
 
 export const WeatherProvider = ({ children }) => {
     const [weatherData, setWeatherData] = useState();
     const [loading, setLoading] = useState(true);
-
+    const [coords, setCoords] = useState({ lat: -33.8679, lon: 151.2073 });
+    51.5074
     useEffect(() => {
-        axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=-33.8679&lon=151.2073&appid={appId}&units=metric').then(
+        const { lat, lon } = coords;
+
+        axios.get(`${WEATHER_API_URL}?lat=${lat}&lon=${lon}&appid=${Configuration.apiKey}&units=metric`).then(
             response => {
                 setWeatherData(response.data);
                 setLoading(false);
             }
         );
-    }, [setLoading]);
+    }, [coords, setLoading, setWeatherData]);
 
     const contentsEl = loading ? <div>Loading...</div> : children;
 
     return (
         <WeatherContext.Provider value={weatherData}>
-            { contentsEl }
+            <LocationContext.Provider value={ { updateLocation: setCoords } }>
+                { contentsEl }
+            </LocationContext.Provider>
         </WeatherContext.Provider>
     )
 }
