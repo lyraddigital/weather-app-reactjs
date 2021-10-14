68313c0da62ae8610f60f927d41c48dd2b518134
lyraddigital <daryl@lyraddigital.com>
2021-10-14 21:56:13 +1100

    Update WeatherProvider.js

--- src/core/WeatherProvider.js
+++ src/core/WeatherProvider.js
@@ -8,7 +8,7 @@ export const WeatherProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=-33.8679&lon=151.2073&appid=63c100967793bce054f43823d77eb8fa&units=metric').then(
+        axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=-33.8679&lon=151.2073&appid={appId}&units=metric').then(
             response => {
                 setWeatherData(response.data);
                 setLoading(false);
