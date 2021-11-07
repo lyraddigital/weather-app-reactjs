6dcd53968b3f480031a278c83d98d7814aa7dbee
darylduck <daryl.duckmanton@gmail.com>
2021-11-08 02:01:36 +1100

    More loader styles

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,9 +3,15 @@ import ClipLoader from 'react-spinners/ClipLoader';
 import style from './Loader.module.scss';
 
 export const Loader = ({ isLoading }) => {
-  return (
+  document.body.style.overflow = isLoading ? 'hidden' : 'auto';
+
+  return isLoading ? (
     <div id={style.loaderOverlay}>
-      <ClipLoader isLoading={isLoading} />
+      <div id={style.loaderContent}>
+        <p>Loading Weather data</p>
+        <ClipLoader size={60} color="#FFF" />
+        <p>Please wait.</p>
+      </div>
     </div>
-  );
+  ) : null;
 };
