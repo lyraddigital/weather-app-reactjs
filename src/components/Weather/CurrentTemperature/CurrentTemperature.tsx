2d83fb26c19a35cd6da8f9365476d186cf687dbc
darylduck <daryl@lyraddigital.com>
2021-12-02 22:17:54 +1100

    Added icons added night time logic

--- src/components/Weather/CurrentTemperature/CurrentTemperature.tsx
+++ src/components/Weather/CurrentTemperature/CurrentTemperature.tsx
@@ -7,22 +7,31 @@ import style from './CurrentTemperature.module.scss';
 interface CurrentTemperatureProps {
   temp?: number;
   weatherId?: number;
+  isNightTime?: boolean;
 }
 
 export const CurrentTemperature = (
   props: CurrentTemperatureProps,
 ): JSX.Element => {
-  const { temp, weatherId } = props;
+  const { temp, weatherId, isNightTime } = props;
   const weatherSummary = getWeatherDescription(weatherId);
 
   return (
     <div className={style.currentTemperature}>
       <div className={style.iconContainer}>
-        <WeatherIcon weatherId={weatherId} className={style.icon} />
+        <WeatherIcon
+          weatherId={weatherId}
+          className={style.icon}
+          isNightTime={isNightTime}
+        />
       </div>
       <div className={style.contentContainer}>
-        <div data-testid="current-weather-temp" className={style.value}>{temp}&deg;</div>
-        <div data-testid="current-weather-summary" className={style.summary}>{weatherSummary}</div>
+        <div data-testid="current-weather-temp" className={style.value}>
+          {temp}&deg;
+        </div>
+        <div data-testid="current-weather-summary" className={style.summary}>
+          {weatherSummary}
+        </div>
       </div>
     </div>
   );
--- src/components/Weather/CurrentWeather/CurrentWeather.tsx
+++ src/components/Weather/CurrentWeather/CurrentWeather.tsx
@@ -1,3 +1,4 @@
+import { isCurrentTimeNight } from 'core';
 import { useCurrentWeather } from 'hooks';
 import { CurrentTemperature, Stats } from 'components/Weather';
 
@@ -5,10 +6,15 @@ import style from './CurrentWeather.module.scss';
 
 export const CurrentWeather = (): JSX.Element => {
   const { currentTemp, weatherId, statistics } = useCurrentWeather();
+  const isNightTime = isCurrentTimeNight(statistics.localTime);
 
   return (
     <div id={style.currentWeather}>
-      <CurrentTemperature temp={currentTemp} weatherId={weatherId} />
+      <CurrentTemperature
+        temp={currentTemp}
+        weatherId={weatherId}
+        isNightTime={isNightTime}
+      />
       <Stats details={statistics} />
     </div>
   );
