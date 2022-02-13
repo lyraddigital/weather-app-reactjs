8ab46f489aa2cbe80b7c5514165da46b7e4f8c31
darylduck <daryl@lyraddigital.com>
2022-02-13 21:38:28 +1100

    Fixed stats test on CI server.

--- src/components/Weather/Stats/Stats.test.tsx
+++ src/components/Weather/Stats/Stats.test.tsx
@@ -1,13 +1,16 @@
 import { render } from '@testing-library/react';
+
 import { WeatherStatistics } from 'models';
 import { Stats } from './Stats';
 
 describe('Stats', () => {
   it('Shows the correct information in the component when all data is set', async () => {
     // Arrange
+    const sunriseTime = new Date(2022, 2, 13, 6, 33);
+    const sunsetTime = new Date(2022, 2, 13, 19, 45);
     const details: WeatherStatistics = {
-      sunriseTime: new Date('2021-11-29T07:30:00Z'),
-      sunsetTime: new Date('2021-11-29T20:30:00Z'),
+      sunriseTime: sunriseTime,
+      sunsetTime: sunsetTime,
       highTemp: 35,
       lowTemp: 19,
       windSpeed: 33,
@@ -31,8 +34,8 @@ describe('Stats', () => {
     expect(lowTempEl.textContent).toBe(`${details.lowTemp}\u00b0`);
     expect(windSpeedEl.textContent).toBe(`${details.windSpeed}km/h`);
     expect(rainPercentageEl.textContent).toBe(`${details.rainPercentage}%`);
-    expect(sunriseTimeEl.textContent).toBe('18:30');
-    expect(sunsetTimeEl.textContent).toBe('7:30');
+    expect(sunriseTimeEl.textContent).toBe('6:33');
+    expect(sunsetTimeEl.textContent).toBe('19:45');
   });
 
   it('Shows blank for sunrise and sunset time as it is not included', async () => {
