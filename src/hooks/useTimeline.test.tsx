842dcb49a6e3b77b79e84c16b716a11fd7bbb3bd
darylduck <daryl@lyraddigital.com>
2022-02-26 16:57:14 +1100

    Latest linter

--- src/hooks/useTimeline.test.tsx
+++ src/hooks/useTimeline.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { getUnixTime } from 'date-fns';
 
 import { formatTime } from 'utilities';
@@ -6,7 +6,7 @@ import { useTimeline } from 'hooks';
 import { WeatherContext } from 'context';
 import {
   TimelinePeriodApiResponse,
-  WeatherApiResponse,
+  WeatherDetailsApiResponse,
   WeatherTimelinePeriod,
 } from 'models';
 
@@ -28,20 +28,24 @@ const TempTimelineChild = () => {
   return <div>{timelinePeriodEls}</div>;
 };
 
-const createTimePeriod = (index: number): TimelinePeriodApiResponse => {
+const createTimePeriod = (
+  index: number,
+  weather?: WeatherDetailsApiResponse,
+): TimelinePeriodApiResponse => {
   return {
     temp: index * 2,
     dt: getUnixTime(Date.UTC(2021, 10, 27)),
-    weather: [{ id: index + 1 }],
+    weather: [weather || { id: index + 1 }],
   };
 };
 
 describe('useTimeline', () => {
-  it('Returns a filtered timeline array when the ApiResponse is defined', async () => {
+  it('Returns a filtered timeline array when the ApiResponse is defined', () => {
     // Arrange
-    const apiResponse: WeatherApiResponse = {
+    const weather = { id: 1 };
+    const apiResponse = {
       hourly: [
-        createTimePeriod(0),
+        createTimePeriod(0, weather),
         createTimePeriod(1),
         createTimePeriod(2),
         createTimePeriod(3),
@@ -49,8 +53,10 @@ describe('useTimeline', () => {
       timezone: 'Australia/Sydney',
     };
 
+    console.log(apiResponse.hourly[3].weather);
+
     // Action
-    const wrapper = render(
+    render(
       <WeatherContext.Provider
         value={{ data: apiResponse, isFirstLoad: false, isLoading: true }}
       >
@@ -58,30 +64,28 @@ describe('useTimeline', () => {
       </WeatherContext.Provider>,
     );
 
+    screen.debug();
+
     // Assert
-    const timelineHourTempEl = await wrapper.findByTestId(
-      `timeline-period-temp-0`,
-    );
-    const timelineHourDtEl = await wrapper.findByTestId(
-      `timeline-period-time-0`,
-    );
-    const timelineHourWheatherIdEl = await wrapper.findByTestId(
+    const timelineHourTempEl = screen.getByTestId(`timeline-period-temp-0`);
+    const timelineHourDtEl = screen.getByTestId(`timeline-period-time-0`);
+    const timelineHourWeatherIdEl = screen.getByTestId(
       `timeline-period-weatherId-0`,
     );
 
     expect(timelineHourTempEl.textContent).toBe(
-      apiResponse.hourly![3]!.temp!.toString(),
+      apiResponse.hourly[3].temp?.toString(),
     );
 
+    console.log(weather.id.toString());
+    console.log(timelineHourWeatherIdEl.textContent);
     expect(timelineHourDtEl.textContent).toBe('2021-11-27 11:00:00');
-    expect(timelineHourWheatherIdEl.textContent).toBe(
-      apiResponse.hourly![3]!.weather![0].id.toString(),
-    );
+    expect(timelineHourWeatherIdEl.textContent).toBe(weather.id.toString());
   });
 
-  it('Returns an empty timeline array when the ApiResponse is not defined', async () => {
+  it('Returns an empty timeline array when the ApiResponse is not defined', () => {
     // Arrange / Action
-    const wrapper = render(
+    render(
       <WeatherContext.Provider
         value={{ data: undefined, isFirstLoad: false, isLoading: true }}
       >
@@ -90,21 +94,21 @@ describe('useTimeline', () => {
     );
 
     // Assert
-    const timelineHourTempEls = await wrapper.queryAllByTestId(
+    const timelineHourTempEls = screen.queryAllByTestId(
       `timeline-period-temp-0`,
     );
 
     expect(timelineHourTempEls.length).toBe(0);
   });
 
-  it('Returns an empty Forecast array when the ApiResponse.hourly object is not defined', async () => {
+  it('Returns an empty Forecast array when the ApiResponse.hourly object is not defined', () => {
     // Arrange
-    const apiResponse: WeatherApiResponse = {
+    const apiResponse = {
       timezone: 'Australia/Sydney',
     };
 
     // Action
-    const wrapper = render(
+    render(
       <WeatherContext.Provider
         value={{ data: apiResponse, isFirstLoad: false, isLoading: true }}
       >
@@ -113,26 +117,26 @@ describe('useTimeline', () => {
     );
 
     // Assert
-    const timelineHourTempEls = await wrapper.queryAllByTestId(
+    const timelineHourTempEls = screen.queryAllByTestId(
       `timeline-period-temp-0`,
     );
 
     expect(timelineHourTempEls.length).toBe(0);
   });
 
-  it('Returns a maximum of 6 timelines starting from the second record to the 19th skipping every 3 hours', async () => {
+  it('Returns a maximum of 6 timelines starting from the second record to the 19th skipping every 3 hours', () => {
     // Arrange
     const hourly = [
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
       21, 22, 23, 24,
     ].map((i: number) => createTimePeriod(i));
-    const apiResponse: WeatherApiResponse = {
+    const apiResponse = {
       hourly,
       timezone: 'Australia/Sydney',
     };
 
     // Action
-    const wrapper = render(
+    render(
       <WeatherContext.Provider
         value={{ data: apiResponse, isFirstLoad: false, isLoading: true }}
       >
@@ -141,50 +145,50 @@ describe('useTimeline', () => {
     );
 
     // Assert
-    const timelinePeriodTempElOne = await wrapper.findByTestId(
+    const timelinePeriodTempElOne = screen.getByTestId(
       `timeline-period-temp-0`,
     );
-    const timelinePeriodTempElTwo = await wrapper.findByTestId(
+    const timelinePeriodTempElTwo = screen.getByTestId(
       `timeline-period-temp-1`,
     );
-    const timelinePeriodTempElThree = await wrapper.findByTestId(
+    const timelinePeriodTempElThree = screen.getByTestId(
       `timeline-period-temp-2`,
     );
-    const timelinePeriodTempElFour = await wrapper.findByTestId(
+    const timelinePeriodTempElFour = screen.getByTestId(
       `timeline-period-temp-3`,
     );
-    const timelinePeriodTempElFive = await wrapper.findByTestId(
+    const timelinePeriodTempElFive = screen.getByTestId(
       `timeline-period-temp-4`,
     );
-    const timelinePeriodTempElSix = await wrapper.findByTestId(
+    const timelinePeriodTempElSix = screen.getByTestId(
       `timeline-period-temp-5`,
     );
-    const timelinePeriodTempSevenEls = await wrapper.queryAllByTestId(
+    const timelinePeriodTempSevenEls = screen.queryAllByTestId(
       `timeline-period-temp-6`,
     );
 
     expect(timelinePeriodTempElOne.textContent).toBe(
-      apiResponse.hourly![3]!.temp!.toString(),
+      apiResponse.hourly[3].temp?.toString(),
     );
 
     expect(timelinePeriodTempElTwo.textContent).toBe(
-      apiResponse.hourly![6]!.temp!.toString(),
+      apiResponse.hourly[6].temp?.toString(),
     );
 
     expect(timelinePeriodTempElThree.textContent).toBe(
-      apiResponse.hourly![9]!.temp!.toString(),
+      apiResponse.hourly[9].temp?.toString(),
     );
 
     expect(timelinePeriodTempElFour.textContent).toBe(
-      apiResponse.hourly![12]!.temp!.toString(),
+      apiResponse.hourly[12].temp?.toString(),
     );
 
     expect(timelinePeriodTempElFive.textContent).toBe(
-      apiResponse.hourly![15]!.temp!.toString(),
+      apiResponse.hourly[15].temp?.toString(),
     );
 
     expect(timelinePeriodTempElSix.textContent).toBe(
-      apiResponse.hourly![18]!.temp!.toString(),
+      apiResponse.hourly[18].temp?.toString(),
     );
 
     expect(timelinePeriodTempSevenEls.length).toBe(0);
