cf0633f680e76f3401f038aa5b0d2b74e8ac3e31
darylduck <daryl@lyraddigital.com>
2022-07-04 21:03:52 +1000

    Try this?

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -5,13 +5,24 @@ import * as cdk from 'aws-cdk-lib';
 import { WebsiteStack } from '../lib/website-stack';
 
 const app = new cdk.App();
-const env = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
 
 // Can I pass in the environment name here rather than have multiple
 // entries hard coded?
-new WebsiteStack(app, 'ReactWeatherStack-Development', { env });
-new WebsiteStack(app, 'ReactWeatherStack-Staging', { env });
-new WebsiteStack(app, 'ReactWeatherStack-Production', { env });
+new WebsiteStack(app, 'ReactWeatherStack-Development', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
+});
+new WebsiteStack(app, 'ReactWeatherStack-Staging', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }  
+});
+new WebsiteStack(app, 'ReactWeatherStack-Production', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
+});
